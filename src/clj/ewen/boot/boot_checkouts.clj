77942a30569a5d201839dc059194e3e37a10fd84
ewen <ewen.grosjean@gmail.com>
2015-03-04 01:26:33 +0100

    Checkouts is now compatible with leiningen projects

--- src/clj/ewen/boot/boot_checkouts.clj
+++ src/clj/ewen/boot/boot_checkouts.clj
@@ -5,50 +5,76 @@
 
 
 
+ (defn get-boot-project-env [project-pod project-dir-path]
+       (let [boot-file-path (-> (io/file project-dir-path "build.boot")
+                                .getAbsolutePath)]
+            (pod/with-eval-in project-pod
+                              (require '[boot.core]
+                                       '[boot.file :as file]
+                                       '[clojure.java.io :as io]
+                                       '[boot.tmpregistry :as tmp]
+                                       '[boot.pod :as pod])
+                              (reset! pod/pod-id 1)
+                              (->> (io/file ~project-dir-path)
+                                   (.getCanonicalFile)
+                                   file/split-path
+                                   rest
+                                   (apply io/file (boot.App/getBootDir) "tmp")
+                                   tmp/registry
+                                   tmp/init!
+                                   (reset! @#'boot.core/tmpregistry))
+                              (doto @#'boot.core/boot-env
+                                    (reset! {:watcher-debounce 10
+                                             :dependencies []
+                                             :directories #{}
+                                             :source-paths #{}
+                                             :resource-paths #{}
+                                             :asset-paths #{}
+                                             :target-path "target"
+                                             :repositories [["clojars" "http://clojars.org/repo/"]
+                                                            ["maven-central" "http://repo1.maven.org/maven2/"]]})
+                                    (add-watch :boot.core/boot #(@#'boot.core/configure!* %3 %4)))
+                              (@#'boot.core/set-fake-class-path!)
+                              (@#'boot.core/temp-dir** nil :asset)
+                              (@#'boot.core/temp-dir** nil :source)
+                              (@#'boot.core/temp-dir** nil :resource)
+                              (@#'boot.core/temp-dir** nil :user :asset)
+                              (@#'boot.core/temp-dir** nil :user :source)
+                              (@#'boot.core/temp-dir** nil :user :resource)
+                              (pod/add-shutdown-hook! @#'boot.core/do-cleanup!)
+                              (binding [*ns* *ns*]
+                                       (ns boot.user)
+                                       (in-ns 'boot.user)
+                                       (require '[boot.core :refer :all])
+                                       (require '[boot.task.built-in :refer :all])
+                                       (load-file ~boot-file-path))
+                              (boot.core/get-env))))
+
+
+
+(defn get-lein-project-env [project-pod project-dir-path]
+      (let [lein-file-path (-> (io/file project-dir-path "project.clj")
+                               .getAbsolutePath)]
+           (->> lein-file-path
+                slurp
+                read-string
+                (drop 2)
+                (cons :version)
+                (apply hash-map))))
+
 (defn get-project-env [project-pod project-dir-path]
-  (let [boot-file-path (-> (io/file project-dir-path "build.boot")
-                           .getAbsolutePath)]
-    (pod/with-eval-in project-pod
-                      (require '[boot.core]
-                               '[boot.file :as file]
-                               '[clojure.java.io :as io]
-                               '[boot.tmpregistry :as tmp]
-                               '[boot.pod :as pod])
-                      (reset! pod/pod-id 1)
-                      (->> (io/file ~project-dir-path)
-                           (.getCanonicalFile)
-                           file/split-path
-                           rest
-                           (apply io/file (boot.App/getBootDir) "tmp")
-                           tmp/registry
-                           tmp/init!
-                           (reset! @#'boot.core/tmpregistry))
-                      (doto @#'boot.core/boot-env
-                        (reset! {:watcher-debounce 10
-                                 :dependencies []
-                                 :directories #{}
-                                 :source-paths #{}
-                                 :resource-paths #{}
-                                 :asset-paths #{}
-                                 :target-path "target"
-                                 :repositories [["clojars" "http://clojars.org/repo/"]
-                                                ["maven-central" "http://repo1.maven.org/maven2/"]]})
-                        (add-watch :boot.core/boot #(@#'boot.core/configure!* %3 %4)))
-                      (@#'boot.core/set-fake-class-path!)
-                      (@#'boot.core/temp-dir** nil :asset)
-                      (@#'boot.core/temp-dir** nil :source)
-                      (@#'boot.core/temp-dir** nil :resource)
-                      (@#'boot.core/temp-dir** nil :user :asset)
-                      (@#'boot.core/temp-dir** nil :user :source)
-                      (@#'boot.core/temp-dir** nil :user :resource)
-                      (pod/add-shutdown-hook! @#'boot.core/do-cleanup!)
-                      (binding [*ns* *ns*]
-                        (ns boot.user)
-                        (in-ns 'boot.user)
-                        (require '[boot.core :refer :all])
-                        (require '[boot.task.built-in :refer :all])
-                        (load-file ~boot-file-path))
-                      (boot.core/get-env))))
+      (let [lein-file (io/file project-dir-path "project.clj")
+            boot-file (io/file project-dir-path "build.boot")]
+           (cond (.exists boot-file)
+                 (get-boot-project-env project-pod project-dir-path)
+                 (.exists lein-file)
+                 (get-lein-project-env project-pod project-dir-path)
+                 :esle (throw
+                         (RuntimeException.
+                           (format
+                             "Cannot find a build.boot file or project.clj file for project \"%s\""
+                             (-> (io/file project-dir-path)
+                                 (.getName))))))))
 
 (deftask checkouts []
          (let [checkouts-pod (pod/make-pod {:dependencies '[[boot/core "2.0.0-rc10"]]})
